f49d4029161921990ba56a5e85d7a5272fba9c3f
Mario Puerto <mariopuerto.nyc@gmail.com>
2021-01-28 16:20:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,8 @@
 def greet_characters(array)
 
 
-    array.each { |element|element.class == String}
-    print [Hello {array}!]
+    array.each {|element|} 
+    print #{Hello r }
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
@@ -13,6 +13,9 @@ end
 
 
 def list_dwarves(array)
+
+  array.each_with_index { |item, index|
+  arrayt[item] = index
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
