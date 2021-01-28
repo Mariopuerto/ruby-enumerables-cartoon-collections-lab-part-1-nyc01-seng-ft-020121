538e8d48777702f3a8c3126d50da6240b44cbef3
Mario Puerto <mariopuerto.nyc@gmail.com>
2021-01-28 15:51:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def greet_characters(array)
 
   greet_characters.each do |greet_characters|
-     puts "# Hello {greet_characters}"
+     puts "#{greet_characters}"
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
