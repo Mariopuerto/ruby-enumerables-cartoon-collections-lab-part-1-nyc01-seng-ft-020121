0fec79972a96253007b5c89ce8c99421c27d4f4d
Mario Puerto <mariopuerto.nyc@gmail.com>
2021-01-28 16:30:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ def list_dwarves(array)
 
 array.each_with_index do |array , num|
 print "#{array , num}"
-end
+
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
