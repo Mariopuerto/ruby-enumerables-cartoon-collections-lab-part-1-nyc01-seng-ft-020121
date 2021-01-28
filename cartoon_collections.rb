fee54515e04913ae161c771cdab437ff7b68c2c3
Mario Puerto <mariopuerto.nyc@gmail.com>
2021-01-28 16:31:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,8 +15,8 @@ end
 
 def list_dwarves(array)
 
-array.each_with_index do |array|
-print "#{array}"
+array.each_with_index do |array , index|
+print "#{array , index}"
 end
   # Use `each_with_index` to enumerate over the provided array
   #
